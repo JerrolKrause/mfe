f3370570d176c7f986f5a2589548e5dce6d4612f
Jerrol <jerrolkrause@gmail.com>
2024-04-02 14:51:45 -0400

    Add sandbox form

--- apps/sandbox/src/app/sandbox.component.ts
+++ apps/sandbox/src/app/sandbox.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { BehaviorSubject, take } from 'rxjs';
+
+interface State {
+  waiting: boolean;
+  success: boolean;
+  error: null | string;
+}
 
 @Component({
   selector: 'app-sandbox',
   templateUrl: './sandbox.component.html',
   styleUrl: './sandbox.component.scss',
 })
-export class SandboxComponent {
-  title = 'sandbox';
+export class SandboxComponent implements OnInit {
+  public state$ = new BehaviorSubject<State>({
+    waiting: false,
+    success: false,
+    error: null,
+  });
+
+  public form = this.fb.group({
+    tbd: ['Surprise me', []],
+  });
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Get item
+    return;
+    /**
+    this.http.get('').subscribe((r) => {
+      console.log(r);
+      this.form.patchValue(r);
+    });
+     */
+  }
+
+  public submit() {
+    const val = this.form.getRawValue();
+    console.log(val);
+    // this.http.post('', val).subscribe();
+  }
+
+  public stateChange(stateNew: Partial<State>) {
+    this.state$
+      .pipe(take(1))
+      .subscribe((stateOld) => this.state$.next({ ...stateOld, ...stateNew }));
+  }
 }
--- apps/sandbox/src/app/sandbox.module.ts
+++ apps/sandbox/src/app/sandbox.module.ts
@@ -1,8 +1,16 @@
 import { MasterpageModule } from '$libs';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { provideClientHydration } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputOtpModule } from 'primeng/inputotp';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 import { SandboxComponent } from './sandbox.component';
 import { SandboxRoutes } from './sandbox.routes';
 
@@ -12,6 +20,14 @@ import { SandboxRoutes } from './sandbox.routes';
     CommonModule,
     RouterModule.forChild(SandboxRoutes),
     MasterpageModule,
+    ReactiveFormsModule,
+    CardModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputOtpModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    ButtonModule,
   ],
   providers: [provideClientHydration()],
   exports: [],
