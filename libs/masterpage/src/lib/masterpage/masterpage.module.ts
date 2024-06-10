4f557789af04f0c2e086559bf948e7819479b2a1
Jerrol Krause <jerrolkrause@gmail.com>
2024-06-10 11:55:46 -0400

    Updated sidebar functionality

--- libs/masterpage/src/lib/masterpage/masterpage.module.ts
+++ libs/masterpage/src/lib/masterpage/masterpage.module.ts
@@ -1,16 +1,17 @@
 import { IconsModule } from '$icons';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AccordionModule } from 'primeng/accordion';
 import { ConfirmationService } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
 import { SidebarModule } from 'primeng/sidebar';
 import { TooltipModule } from 'primeng/tooltip';
 import { HeaderComponent } from './header/header.component';
 import { MasterpageComponent } from './masterpage.component';
 import { NavComponent } from './nav/nav.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
 @NgModule({
   imports: [
     CommonModule,
@@ -19,6 +20,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     IconsModule,
     TooltipModule,
     AccordionModule,
+    ReactiveFormsModule,
+    InputTextModule,
   ],
   declarations: [
     MasterpageComponent,
